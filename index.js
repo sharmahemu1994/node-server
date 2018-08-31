7161e26e8708b4aceb0ebbe78f1e12323e6c574e
himanshu <sharma.hemu1994@gmail.com>
2018-08-31 21:34:02 +0530

    added debuggig example

--- index.js
+++ index.js
@@ -1,12 +1,48 @@
+// /**
+//  * primary file for API
+//  *  
+//  */
+
+//  // Dependencies
+
+// const server = require('./server');
+// const cli = require('./lib/cli');
+
+// // container for app
+
+// const app = {};
+
+// // Init function
+
+// app.init = () => {
+// 	// start server
+// 	server.init();
+
+// 	// star server CLI
+// 	setTimeout(() => {
+// 		cli.init();
+// 	}, 100);
+// };
+
+// // execute init function
+
+// app.init();
+
+// // export module
+// module.exports = app;
+
+
 /**
  * primary file for API
- *  
+ * clusterd index.js
  */
 
- // Dependencies
+// Dependencies
 
 const server = require('./server');
 const cli = require('./lib/cli');
+const cluster = require('cluster');
+const os = require('os');
 
 // container for app
 
@@ -15,13 +51,23 @@ const app = {};
 // Init function
 
 app.init = () => {
-	// start server
-	server.init();
+	if (cluster.isMaster) {
+		// if we are on the master thread start other process and cli
+		// star server CLI
+		setTimeout(() => {
+			cli.init();
+		}, 100);
 
-	// star server CLI
-	setTimeout(() => {
-		cli.init();
-	}, 100);
+		// fork the process
+		for (let i = 0; i < os.cpus().length; i++) {
+			cluster.fork();
+		}
+	} else {
+		// if we are not on the master thread
+		// start server
+		server.init();	
+	}
+	
 };
 
 // execute init function
